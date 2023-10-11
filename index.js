9f4a9d52173c66d7f00d0357684baa97f0d45687
igor <igor@gmail.com>
2023-10-11 21:55:34 +0200

    drugi komit

--- index.js
+++ index.js
@@ -4,12 +4,25 @@ const app = express()
 
 app.engine("handlebars", exHb.engine({defaultLayout: "main"}))
 app.set("view engine", "handlebars")
+app.use(express.static('public'))
 
 app.get("/", (req, res)=>{
     res.render("home", {title: "Strona glowna"})
 })
-app.get("/huj", (req, res)=>{
-    res.send("to jest huj")
+app.get("/types", (req, res)=>{
+    res.render("types", {title: "Types"})
+})
+app.get("/cart", (req, res)=>{
+    res.render("cart", {title: "Cart"})
+})
+app.get("/help", (req, res)=>{
+    res.render("help", {title: "Help"})
+})
+app.get("/about", (req, res)=>{
+    res.render("about", {title: "About"})
+})
+app.get("/Contact", (req, res)=>{
+    res.render("Contact", {title: "Contact"})
 })
 
 
